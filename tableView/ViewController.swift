c749667454ce36ec363c4aa9afcca287636d3678
Rifat Monzur <rifat@Rifats-Mac-mini.local>
2017-10-16 18:46:54 -0700

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Rifat Monzur on 16/10/17.
+//  Copyright © 2017 Rifat Monzur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
